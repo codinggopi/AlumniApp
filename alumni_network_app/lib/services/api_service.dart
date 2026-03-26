7d9b2e9a8253e0cacb4416ef703ccc8c73502ec0
GOPINATH G <gajagopi2006@gmail.com>
2026-03-26 22:49:40 +0530

    Update api_service.dart

--- alumni_network_app/lib/services/api_service.dart
+++ alumni_network_app/lib/services/api_service.dart
@@ -3,9 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiService {
-  //static const String baseUrl = 'https://alumniapp-qths.onrender.com'; // Change to IP for physical device
-  static const String baseUrl =
-      'https://callum-unstigmatic-yappingly.ngrok-free.dev'; // Change to IP for physical device
+  static const String baseUrl = 'https://alumniapp-qths.onrender.com'; // Change to IP for physical device
+  //static const String baseUrl = 'https://callum-unstigmatic-yappingly.ngrok-free.dev'; // Change to IP for physical device
       
   final _storage = const FlutterSecureStorage();
 
